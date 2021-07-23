143a458cfbe5ecdcddb3d8a01677305600bb67b4
Alejandra Zatarain <azatarainalp@gmail.com>
2021-07-22 19:13:14 -0700

    1st iteration

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,11 +1,33 @@
 let $cart = document.querySelector('#cart tbody');
 let $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
+
+// Got html elements 
+
+
+
+// console.log($product);
+// console.log(quantity);
+// console.log(typeof(subtot));
+
+
+function updateSubtot($cart) {
   // Iteration 1.1
+  let $product = parseFloat($cart.querySelector('.pu span').innerHTML);
+  let quantity = parseFloat($cart.querySelector('.qty input').value);
+  let subtot = parseFloat($cart.querySelector('.subtot span').innerHTML);
+  let subtotSpan = $cart.querySelector('.subtot span');
+
+  subtot = quantity *  $product;
+
+  subtotSpan.innerHTML = `${subtot}`;
+  
+
+  
 }
 
 function calcAll() {
   // Iteration 1.2
+  updateSubtot($cart);
 }
 $calc.onclick = calcAll;
